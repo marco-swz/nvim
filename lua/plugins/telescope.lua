e57cdbed51fa3d8d79af13061ebbf60a5d27bfe1
xRetry <marcoschwarz.at@gmail.com>
2024-02-10 22:37:46 +0100

    Rework telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,29 +7,29 @@ return {
 		local builtin2 = require('telescope.builtin')
 		local keymap = vim.keymap.set
 
-		keymap('n', '<space>fh', builtin2.lsp_workspace_symbols, {})
+		keymap('n', '<space>fy', builtin2.live_grep, {})
+		keymap('n', '<space>fu', builtin2.lsp_references, {})
+		keymap('n', '<space>fi', builtin2.lsp_implementations, {})
+		keymap('n', '<space>fo', builtin2.lsp_definitions, {})
+		keymap('n', '<space>fp', builtin2.lsp_type_definitions, {})
+
+		keymap('n', '<space>fh', builtin2.current_buffer_fuzzy_find, {})
 		keymap('n', '<space>fj', builtin2.find_files, {})
 		keymap('n', '<space>fk', builtin2.diagnostics, {})
 		keymap('n', '<space>fl', builtin2.git_files, {})
 		keymap('n', '<space>fb', builtin2.commands, {})
 		keymap('n', '<space>f;', builtin2.commands, {})
-					  
-		keymap('n', '<space>fy', builtin2.buffers, {})
-		keymap('n', '<space>fu', builtin2.lsp_references, {})
-		keymap('n', '<space>fi', builtin2.lsp_implementations, {})
-		keymap('n', '<space>fo', builtin2.grep_string, {})
-		keymap('n', '<space>fp', builtin2.lsp_document_symbols, {})
-								   
-		keymap('n', '<space>fn', builtin2.jumplist, {})
+	  
+		keymap('n', '<space>fn', builtin2.buffers, {})
 		keymap('n', '<space>fm', builtin2.marks, {})
 		keymap('n', '<space>f,', builtin2.keymaps, {})
 		keymap('n', "<space>f.", builtin2.resume, {})
 
 		require('telescope').setup{
 			file_ignore_patterns = {
-				'.git/',
-				'.cache/',
-				'.history/',
+				'.git',
+				'.cache',
+				'.history',
 				'%.zip',
 				'%.png',
 				'%.PNG',
@@ -41,7 +41,15 @@ return {
 				'%.class',
 				'%.pdf',
 				'%.exe',
-			}
+			},
+
+            defaults = {
+                layout_strategy = 'horizontal',
+                layout_config = {
+                    prompt_position = 'top',
+                },
+                sorting_strategy = "ascending",
+            },
 		}
 	end
 }
